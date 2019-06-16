034d4697eae57a0239deec950d252c2b6b738402
ajos3ph <andre.cr.vg@hotmail.com>
2019-06-16 17:13:55 -0300

    Add files via upload

---
+++ Alunos/src/alunos/aluno.java
@@ -0,0 +1,65 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package alunos;
+
+
+public class aluno {
+    //Atributos da Classe
+    private String cpd;
+    private String nome;
+    private String curso;
+    private String semestre1;
+    private String semestre2;
+
+    //Gets e Sets
+    public String getCpd() {
+        return cpd;
+    }
+
+    public void setCpd(String cpd) {
+        this.cpd = cpd;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCurso() {
+        return curso;
+    }
+
+    public void setCurso(String curso) {
+        this.curso = curso;
+    }
+
+    public String getSemestre1() {
+        return semestre1;
+    }
+
+    public void setSemestre1(String semestre1) {
+        this.semestre1 = semestre1;
+    }
+
+    public String getSemestre2() {
+        return semestre2;
+    }
+
+    public void setSemestre2(String semestre2) {
+        this.semestre2 = semestre2;
+    }
+
+    void setCPD(String text) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+    
+    
+    
+}
